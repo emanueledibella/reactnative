2119af70a4815a4310fab651fe763d0edfa93b55
Emanuele Di Bella <55705095+emanueledibella@users.noreply.github.com>
2022-07-08 23:05:15 +0200

    new components

--- App.js
+++ App.js
@@ -1,22 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  StyleSheet,
+  ScrollView,
+  Text, 
+  View, 
+  SafeAreaView, 
+  Image, 
+  TouchableWithoutFeedback, 
+  TouchableOpacity,  
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert
+} from 'react-native';
+// SafeAreaView per evitare il notche
+// Text -> props: numberOfLines={1}
 export default function App() {
-  
-  var x = 1;
-  
+
+  const press = () => alert('press')
+//style={styles.container}
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    <ScrollView >
       <StatusBar style="auto" />
-    </View>
+      <Text numberOfLines={1} onPress={press}>Immagine TouchableWithoutFeedback</Text>
+      <TouchableWithoutFeedback onPress={() => alert('Tapped')} onLongPress={() => alert('Long Tapped')}>
+        <Image 
+          resizeMode={'contain'}
+          fadeDuration={1000}
+          blurRadius={10}
+          source={
+            {
+              uri: "https://picsum.photos/200/300",
+              width: 100,
+              height: 300
+            }
+        }/> 
+      </TouchableWithoutFeedback>
+      <Text numberOfLines={1} onPress={press}>Immagine TouchableOpacity</Text>
+      <TouchableOpacity onPress={() => alert('Tapped')} onLongPress={() => alert('Long Tapped')}>
+        <Image 
+          resizeMode={'contain'}
+          fadeDuration={1000}
+          blurRadius={10}
+          source={
+            {
+              uri: "https://picsum.photos/200/300",
+              width: 100,
+              height: 300
+            }
+        }/> 
+      </TouchableOpacity>
+      <Text numberOfLines={1} onPress={press}>Immagine TouchableHighlight</Text>
+      <TouchableHighlight onPress={() => alert('Tapped')} onLongPress={() => alert('Long Tapped')}>
+        <Image 
+          resizeMode={'contain'}
+          fadeDuration={1000}
+          blurRadius={10}
+          source={
+            {
+              uri: "https://picsum.photos/200/300",
+              width: 100,
+              height: 300
+            }
+        }/> 
+      </TouchableHighlight>
+      <Text numberOfLines={1} onPress={press}>Immagine TouchableNativeFeedback (android solo)</Text>
+      <TouchableNativeFeedback onPress={() => alert('Tapped')} onLongPress={() => alert('Long Tapped')}>
+        <View style={{width: 200, height: 70, backgroundColor: 'dodgerblue'}}></View>
+      </TouchableNativeFeedback>
+    <Button 
+      color="orange"
+      title="Alert.alert" onPress={() => Alert.alert('My Title', 'Messaggio', [
+        {text: "Yes", onPress: () => Alert.alert('YES')},
+        {text: "No", onPress: () => Alert.alert('No')}
+      ])}/>
+     <Button 
+      color="orange"
+      title="Alert.prompt(Solo Ios)" onPress={() => Alert.prompt("My Title", "Messaggio", (text) => Alert.alert(text))}/>
+    </ScrollView>
   );
 }
+const container = {backgroundColor: 'orange'};
 
+// si usa con style={styles.container} oppure style={[styles.container], [styles.container2]} per includere 2 stili
+// Create permette di fare la validazione dele proprietà e include alcune ottimizzazioni
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
